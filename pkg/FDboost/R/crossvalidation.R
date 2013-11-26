65c43cb841a22d6942452b3318a5d922fe6dcb85
sbrockhaus <sbrockhaus@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-11-26 17:37:53 +0000

    cvMa() is only a wrapper to cv() that expands the folds
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fdboost@48 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/FDboost/R/crossvalidation.R
+++ pkg/FDboost/R/crossvalidation.R
@@ -838,82 +838,31 @@ cvMa <- function(ydim=NULL, Y=NULL, weights=NULL,
                  type = c("bootstrap", "kfold", "subsampling"), 
                  B = ifelse(type == "kfold", 10, 25), prob = 0.5, strata = NULL, id=NULL){
   
-  ####################### functions taken from mboost
-  # Weights for cross-validation and boosting
-  # functions for cross-validation and bootstrap weights, 
-  # taken from the package mboost
-  cvkfold <- function (n, k){
-    if (k > n/2) 
-      stop("k > n/2")
-    fl <- floor(n/k)
-    folds <- c(rep(c(rep(0, fl), rep(1, n)), k - 1), rep(0, n * k - (k - 1) * (fl + n)))
-    matrix(folds, nrow = n)[sample(1:n), , drop = FALSE]
-  }
-  cvboot <- function(n, B, weights) rmultinom(B, n, weights/sum(weights))
-  
-  cvsub <- function(n, prob, B) {
-    k <- floor(n * prob)
-    indx <- rep(c(0, 1), c(n - k, k))
-    replicate(B, sample(indx))[sample(1:n),, drop = FALSE]
-  }
-  #######################
-  
   if(is.null(ydim) & is.null(Y)) stop("You have to specify Y or ydim")
   
   ncolY <- if(is.null(ydim)) ncol(Y) else ydim[2]
   nrowY <- if(is.null(ydim)) nrow(Y) else ydim[1]  
-  if(is.null(weights)) weights <- rep(1, nrowY)
+  if(is.null(weights)) weights <- rep(1, nrowY*ncolY)
   
   type <- match.arg(type)
   n <- length(weights)
   
-  if (nrowY!=n) stop("Weights and Y/ydim have to match")
-  if (is.null(strata)) strata <- gl(1, n)
-  if (!is.factor(strata)) stop(sQuote("strata"), " must be a factor")
-  
-  
-  # sample individual trajectories
-  if(is.null(id)){
-    folds <- matrix(0, nrow = n, ncol = B)
-    for (s in levels(strata)) {
-      indx <- which(strata == s)
-      folds[indx, ] <- switch(type, 
-                              bootstrap = cvboot(length(indx), B = B, weights[indx]), 
-                              kfold = cvkfold(length(indx),k = B) * weights[indx], 
-                              subsampling = cvsub(length(indx), prob = prob, 
-                                                          B = B)*weights[indx])
-    } 
-    # sample on level of id
-  }else{
-    folds <- matrix(0, nrow = length(unique(id)), ncol = B)
-    for (s in levels(strata)) {
-      indx <- which(strata == s)
-      ids <- id[which(strata == s)] # id in strata
-      ids1 <- unique(ids)
-      weightids <- as.numeric(table(ids))
-      indx <- 1:length(ids1)
-      folds[indx, ] <- switch(type, 
-                              bootstrap = cvboot(length(indx), B = B, weightids), 
-                              kfold = cvkfold(length(indx), k = B) * weightids, 
-                              subsampling = cvsub(length(indx), prob = prob, 
-                                                          B = B) * weightids)
-    }
-    # expand folds over ids
-    if(length(unique(weightids))==1){ # equal design
-      folds <- apply(folds, 2, function(x) rep(x, each=weightids[1]))
-    }else{
-      foldsLong <- c()
-      for(i in 1:length(unique(id))){
-        foldsLong <- rbind(foldsLong, apply(folds[i,,drop=FALSE], 2, 
-                                            function(x) rep(x, each=weightids[i])))
-      }
-      folds <- foldsLong
-    }        
-  }
-  
+  if ( (nrowY*ncolY) != n) stop("Weights and Y/ydim have to match")
+ # if (is.null(strata)) strata <- gl(1, n)
+ # if (!is.factor(strata)) stop(sQuote("strata"), " must be a factor")
+    
   # expand folds over the functional measures of the response
   # foldsMa <- apply(folds, 2, function(x) rep(x, each=ncolY))
-  foldsMa <- apply(folds, 2, function(x) rep(x, times=ncolY))
+  if(is.null(id)){
+    folds <- cv(weights=rep(1, nrowY), type = type, B = B, prob = prob, strata = strata)
+    #foldsMa <- apply(folds, 2, function(x) rep(x, times=ncolY))*weights #the same
+    foldsMa <- folds[rep(1:nrowY, times=ncolY), ]*weights
+  }else{
+    stopifnot(length(id)==nrowY)
+    folds <- cv(weights=rep(1, length(unique(id))), type = type, B = B, prob = prob, strata = strata)
+    folds <- folds[id, ] # reapeat folds according to id
+    foldsMa <- folds[rep(1:nrowY, times=ncolY), ]*weights
+  }
   attr(foldsMa, "type") <- paste(B, "-fold ", type, sep = "")
   
   return(foldsMa)
