5ffa768ae96b28d20b62c01b6f8899facdbe58ff
sbrockhaus <sbrockhaus@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-04-13 15:08:07 +0000

    adapt plots
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fdboost@238 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/FDboost/inst/sim/run_time.R
+++ pkg/FDboost/inst/sim/run_time.R
@@ -43,7 +43,7 @@ res <- try(doSimFDboost(settings=settingsTime, oneRepTime))
 save(res, file=paste(pathResults, "resTime.Rdata", sep=""))
 
 res$M <- factor(res$M)
-res$Gy <- factor(res$Gy)
+res$G <- factor(res$Gy)
 
 res$mod <- factor(res$model-2, levels=c(0,1), labels = c("FLAM", "long"))
 
@@ -54,7 +54,7 @@ library(ggplot2)
 pdf("arrayLongTime.pdf", width=9, height=7)
 p <- ggplot(res, aes(y=time.elapsed, x=M, fill=mod)) 
 p + geom_boxplot(aes(colour = mod), outlier.size=.6) +
-  facet_grid( ~ Gy, labeller="label_both") + 
+  facet_grid( ~ G, labeller="label_both") + 
   scale_colour_manual('mod', values = c('FLAM' = 'black', 'long' = 'grey30')) +
   scale_fill_manual(values=c("grey80", "grey95")) + 
   scale_y_continuous(breaks=c(1, 2, 5, 10, 20, 60, 120, 300, 600, 1200, 2700, 
@@ -62,7 +62,9 @@ p + geom_boxplot(aes(colour = mod), outlier.size=.6) +
                      trans="log10",
                      labels=c("1s", "2s", "5s", "10s", "20s", "1 min", "2 min", "5 min", 
                               "10 min", "20 min", "45 min", "90 min", "3h", "6h", "12h")) +
-  labs(title="Computation time", x="N", y="time") +
+  labs(title="", x="N", y="time") +
+  theme(legend.position="top", legend.direction="horizontal", 
+        text=element_text(size = 25), legend.title=element_blank()) + 
   theme(text=element_text(size = 20)) #+ theme_bw()
 dev.off()
 
@@ -70,11 +72,13 @@ dev.off()
 pdf("arrayLongMemory.pdf", width=9, height=7)
 p <- ggplot(res, aes(y=memory, x=M, fill=mod)) 
 p + geom_boxplot(aes(colour = mod), outlier.size=.6) +
-  facet_grid( ~ Gy, labeller="label_both") + 
+  facet_grid( ~ G, labeller="label_both") + 
   scale_colour_manual('mod', values = c('FLAM' = 'black', 'long' = 'grey30')) +
   scale_fill_manual(values=c("grey80", "grey95")) + 
- scale_y_continuous(trans="log10") +
-  labs(title="Memory Consumption", x="N", y="memory") +
+  scale_y_continuous(trans="log10") +
+  labs(title="", x="N", y="memory") +
+  theme(legend.position="top", legend.direction="horizontal", 
+        text=element_text(size = 25), legend.title=element_blank()) +
   theme(text=element_text(size = 20)) #+ theme_bw()
 dev.off()
 
